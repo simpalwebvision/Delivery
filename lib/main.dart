2c829e06b545b70d516511f7b78530d5265b5554
Simpal Yadav <99637496+simpalwebvision@users.noreply.github.com>
2023-06-21 12:39:10 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:app/view/main_tabview/main_tabview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: const MainTabView(),
+    );
+  }
+}
---
+++ lib/view/login/new_password_view.dart
@@ -0,0 +1,71 @@
+import 'package:app/common/color_extension.dart';
+import 'package:app/common_widget/round_button.dart';
+import 'package:flutter/material.dart';
+
+import '../../common_widget/round_textfield.dart';
+
+class NewPasswordView extends StatefulWidget {
+  const NewPasswordView({super.key});
+
+  @override
+  State<NewPasswordView> createState() => _NewPasswordViewState();
+}
+
+class _NewPasswordViewState extends State<NewPasswordView> {
+  TextEditingController txtPassword = TextEditingController();
+  TextEditingController txtConfirmPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              Text(
+                "New Password",
+                style: TextStyle(
+                    color: TColor.primaryText,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Please enter your new password",
+                style: TextStyle(
+                    color: TColor.secondaryText,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              RoundTextfield(
+                hintText: "New Password",
+                controller: txtPassword,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Confirm Password",
+                controller: txtPassword,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              RoundButton(title: "Next", onPressed: () {}),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
